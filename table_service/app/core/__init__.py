eee97d06a58651121ca68a9e0c2bae9fd8410974
AssemblerBossss <nazarovmisa39@mail.ru>
2026-07-11 20:29:57 +0300

    feat: добавить стриминг строк таблицы для асинхронного экспорта
    - Добавлен метод stream_rows_by_table_id в DataRepository
    - Метод использует SQLAlchemy stream + yield_per для чанкового чтения
    - Возвращает асинхронный генератор для потоковой обработки больших таблиц
    - Экспортирован export_storage в core/__init__.py

--- table_service/app/core/__init__.py
+++ table_service/app/core/__init__.py
@@ -1,10 +1,12 @@
 from .database import Base, get_db_session, AsyncSessionFactory
 from .settings import app_settings
 from .rabbitmq import get_user_validator
+from .export_storage import export_storage
 from .logging_config import setup_service_logging
 from .user_event_consumer import user_event_consumer
 from .redis_client import get_redis_client, close_redis_client
 from .elastic import init_es_index, close_es_client, get_es_client
+
 
 __all__ = [
     "Base",
@@ -19,4 +21,5 @@ __all__ = [
     "close_es_client",
     "init_es_index",
     "get_es_client",
+    "export_storage",
 ]
