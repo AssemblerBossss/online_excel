fdd1df0388e417c17a4490dd3b47282a44c9daf5
AssemblerBossss <nazarovmisa39@mail.ru>
2026-07-29 21:29:04 +0300

    feat(repository): добавлена memory-реализация NotificationRepository
    - реализованы методы Create, GetByID, List
    - используется sync.Mutex для потокобезопасности
    - хранение в map[string]*domain.Notification
    - обработка ошибки при отсутствии уведомления

---
+++ notification_service/internal/repository/memory/notification.go
@@ -0,0 +1,49 @@
+package memory
+
+import (
+	"context"
+	"errors"
+	"notification_service/internal/domain"
+	"sync"
+)
+
+type NotificationRepository struct {
+	mu   sync.Mutex
+	data map[string]*domain.Notification
+}
+
+func NewNotificationRepository() *NotificationRepository {
+	return &NotificationRepository{
+		data: make(map[string]*domain.Notification),
+	}
+}
+
+func (r *NotificationRepository) Create(ctx context.Context, notification *domain.Notification) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.data[notification.ID] = notification
+	return nil
+}
+
+func (r *NotificationRepository) GetByID(ctx context.Context, id string) (*domain.Notification, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	notification, ok := r.data[id]
+	if !ok {
+		return nil, errors.New("notification not found")
+	}
+	return notification, nil
+}
+
+func (r *NotificationRepository) List(ctx context.Context) ([]*domain.Notification, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	result := make([]*domain.Notification, 0, len(r.data))
+
+	for _, notification := range r.data {
+		result = append(result, notification)
+	}
+	return result, nil
+}
