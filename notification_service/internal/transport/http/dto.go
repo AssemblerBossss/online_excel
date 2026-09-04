9b3e9853dc7b123862be74d5aed7b916181787d1
AssemblerBossss <nazarovmisa39@mail.ru>
2026-09-04 13:37:39 +0300

    feat(http): добавлены DTO для API с валидацией и JSON-тегами"

--- notification_service/internal/transport/http/dto.go
+++ notification_service/internal/transport/http/dto.go
@@ -1,11 +1,33 @@
 package http
 
-import "notification_service/internal/domain"
+import (
+	"notification_service/internal/domain"
+	"time"
+)
 
 type CreateNotificationRequest struct {
-	UserID    string
-	Channel   domain.NotificationChannel
-	Recipient string
-	Subject   string
-	Body      string
+	UserID    string                     `json:"user_id" validate:"required"`
+	Channel   domain.NotificationChannel `json:"channel" validate:"required,oneof=email push"`
+	Recipient string                     `json:"recipient" validate:"required"`
+	Subject   string                     `json:"subject" validate:"required" `
+	Body      string                     `json:"body" validate:"required"`
+}
+type NotificationResponse struct {
+	ID        string                     `json:"id"`
+	UserID    string                     `json:"user_id"`
+	Channel   domain.NotificationChannel `json:"channel"`
+	Recipient string                     `json:"recipient"`
+	Subject   string                     `json:"subject"`
+	Body      string                     `json:"body"`
+	Status    string                     `json:"status"`
+	CreatedAt time.Time                  `json:"created_at"`
+	SentAt    *time.Time                 `json:"sent_at"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type ListNotificationsResponse struct {
+	Items []*NotificationResponse `json:"items"`
 }
